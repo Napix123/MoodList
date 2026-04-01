27906e9acdf83988104225e5acb6abb8f8d525f7
Nikola Dragutinovic <nikoladragutinovic@192.168.1.14>
2026-04-01 22:25:02 +0200

    Added the session middleware

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from config import FRONTEND_URL, validate_config
 from routers import auth
+import os
+
+SESSION_SECRET_KEY = os.getenv("SESSION_SECRET_KEY")
 
 # Validate the configuration at startup
 validate_config()
 
 app = FastAPI(title="Moodlist API", version="1.0")
 
+app.add_middleware(SessionMiddleware, secret_key=f"`{SESSION_SECRET_KEY}`")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[FRONTEND_URL],
